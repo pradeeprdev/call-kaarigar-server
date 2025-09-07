2057ceb7652cb6824f0896bb7e2051f3870732cc
dheer152004 <dheerajkumar152004@outlook.com>
2025-09-07 14:21:44 +0530

    changes are making

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const User = require('../models/User');
+const User = require('../modules/user/user.model');
 
 // Protect routes
 exports.protect = async (req, res, next) => {
